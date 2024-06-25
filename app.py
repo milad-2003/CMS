89a3912c436209f7efb7a0b55c43317e7685c954
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-06-25 23:37:35 +0330

    Creating the database and adding csv records to it

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -10,4 +13,26 @@ def index():
 
 
 if __name__ == "__main__":
+
+    # |==================================================================================================|
+    # |We use the following code to create the database and add the csv records to it by running it once:|
+    # |==================================================================================================|
+
+    # with app.app_context():
+    #     db.create_all()
+
+
+    # import pandas as pd
+    # import sqlite3
+
+    # connection = sqlite3.connect("instance/data.db")
+
+    # images = pd.read_csv("csv/images.csv")
+    # articles = pd.read_csv("csv/articles.csv")
+
+    # images.to_sql('images', connection, index=False)
+    # articles.to_sql('articles', connection, index=False)
+
+    # connection.close()
+
     app.run(debug=True)
