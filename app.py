d39a8c589780392c711fc55ad08019ca913260d0
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-06-28 03:05:27 +0330

    Writing the logic for getting images based on their category

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import render_template
+from flask import render_template, request
 from config import app, db
 from import_data import *
 from models import *
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/images')
+@app.route('/images', methods=['GET', 'POST'])
 def images():
-    return render_template('images.html')
+    selected_category = request.form.get('category', 'All')
+    categories = db.session.query(Image.category.distinct()).all()
+    categories = [category[0] for category in categories]
+
+    if selected_category == 'All':
+        images = Image.query.all()
+    else:
+        images = Image.query.filter_by(category=selected_category).all()
+
+    return render_template('images.html', images=images, categories=categories, selected_category=selected_category)
+
 
 
 @app.route('/articles')
