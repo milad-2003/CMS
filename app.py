53f6bf184b2fd34d78b4cdb5f12a30c865b2febd
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-06-27 18:16:13 +0330

    Linking navbar items and fixing images.html bug

--- app.py
+++ app.py
@@ -13,6 +13,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/images')
+def images():
+    return render_template('images.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/writer-photographer')
+def writer_photographer():
+    return render_template('writer-photographer.html')
+
+
 if __name__ == "__main__":
     if is_database_empty():
         import_data(app, db)
