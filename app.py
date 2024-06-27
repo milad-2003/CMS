428d972c5d24da9071f3fe74787816bde2aedd5e
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-06-27 21:37:38 +0330

    Creating models and modifying "import_data" to work with it

--- app.py
+++ app.py
@@ -5,7 +5,58 @@ from import_data import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+
+
+class Photographer(db.Model):
+    __tablename__ = 'photographers'
+    photographer_code = db.Column(db.String(10), primary_key=True)
+    photographer_name = db.Column(db.String(255), nullable=False)
+    photographer_email = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Photographer {self.photographer_name}>'
+
+
+class Writer(db.Model):
+    __tablename__ = 'writers'
+    writer_code = db.Column(db.String(10), primary_key=True)
+    writer_name = db.Column(db.String(255), nullable=False)
+    writer_email = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<Writer {self.writer_name}>'
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+    i_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    image_path = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    tags = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.String(255), nullable=True)
+    category = db.Column(db.String(255), nullable=False)
+    photographer_code = db.Column(db.String(10), db.ForeignKey('photographers.photographer_code'), nullable=False)
+    photographer = db.relationship('Photographer', backref=db.backref('images', lazy=True))
+
+    def __repr__(self):
+        return f'<Image {self.title}>'
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+    a_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    content = db.Column(db.String(255), nullable=True)
+    keywords = db.Column(db.String(255), nullable=True)
+    title = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(255), nullable=False)
+    writer_code = db.Column(db.String(10), db.ForeignKey('writers.writer_code'), nullable=False)
+    writer = db.relationship('Writer', backref=db.backref('articles', lazy=True))
+
+    def __repr__(self):
+        return f'<Article {self.title}>'
 
 
 @app.route('/')
@@ -30,7 +81,7 @@ def writer_photographer():
 
 if __name__ == "__main__":
     if is_database_empty():
-        import_data(app, db)
+        import_data(app, db, Photographer, Writer, Image, Article)
     
     app.run(debug=True)
 
--- import_data.py
+++ import_data.py
@@ -1,93 +1,79 @@
-import pandas as pd
-import sqlite3
+import csv
+import os
 
 
 def is_database_empty():
-    import os
-
-
     database_path = os.path.join(os.path.dirname(__file__), 'instance', 'data.db')
     if not os.path.exists(database_path):
         return True
     else:
-        return False
+        return False 
 
- 
-def import_data(app, db):
 
-    # Create the database
+photographers_data = []
+images_data = []
+def read_csv_images(filename):
+    seen_photographer_codes = set()
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            image_data = {
+                'image_path': row['image_path'],
+                'title': row['title'],
+                'tags': row['tags'],
+                'description': row['description'],
+                'category': row['category'],
+                'photographer_code': row['photographer_code']
+            }
+            images_data.append(image_data)
+
+            photographer_code = row['photographer_code']
+            if photographer_code not in seen_photographer_codes:
+                photographer_data = {
+                'photographer_code': row['photographer_code'],
+                'photographer_name': row['photographer_name'],
+                'photographer_email': row['photographer_email']
+            }
+                photographers_data.append(photographer_data)
+                seen_photographer_codes.add(photographer_code)
+
+
+writers_data = []
+articles_data = []
+def read_csv_articles(filename):
+    seen_writer_codes = set()
+    with open(filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            article_data = {
+                'content': row['content'],
+                'keywords': row['keywords'],
+                'title': row['title'],
+                'category': row['category'],
+                'writer_code': row['writer_code']
+            }
+            articles_data.append(article_data)
+
+            writer_code = row['writer_code']
+            if writer_code not in seen_writer_codes:
+                writer_data = {
+                    'writer_code': writer_code,
+                    'writer_name': row['writer_name'],
+                    'writer_email': row['writer_email']
+                }
+                writers_data.append(writer_data)
+                seen_writer_codes.add(writer_code)
+
+
+
+def import_data(app, db, Photographer, Writer, Image, Article):
+    read_csv_images("csv/images.csv")
+    read_csv_articles("csv/articles.csv")
+    
     with app.app_context():
         db.create_all()
-
-    # Create a connection
-    connection = sqlite3.connect("instance/data.db")
-
-    # Read the csv files using pandas
-    images = pd.read_csv("csv/images.csv")
-    articles = pd.read_csv("csv/articles.csv")
-
-    # Adding the records of csv files into the database
-    images.to_sql('images', connection, index=False)
-    articles.to_sql('articles', connection, index=False)
-
-    # Getting the information of photographers and writers
-    photographers = connection.execute("SELECT DISTINCT photographer_code, photographer_name, photographer_email FROM images")
-    writers = connection.execute("SELECT DISTINCT writer_code, writer_name, writer_email FROM articles")
-
-    # Creating a table for photographers and writers
-    connection.execute("""CREATE TABLE photographers (
-                    photographer_code VARCHAR(10),
-                    photographer_name VARCHAR(255),
-                    photographer_email VARCHAR(255),
-                    PRIMARY KEY(photographer_code)
-                    )""")
-    connection.execute("""CREATE TABLE writers (
-                    writer_code VARCHAR(10),
-                    writer_name VARCHAR(255),
-                    writer_email VARCHAR(255),
-                    PRIMARY KEY(writer_code)
-                    )""")
-
-    # Adding the information of photographers and writers into their tables
-    for photographer in photographers:
-        connection.execute(f"INSERT INTO photographers VALUES {photographer}")
-    for writer in writers:
-        connection.execute(f"INSERT INTO writers VALUES {writer}")
-
-    # Removing the extra columns of the images and articles table
-    connection.execute("""CREATE TABLE new_images (
-                    i_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    image_path VARCHAR(255),
-                    title VARCHAR(255),
-                    tags VARCHAR(255),
-                    description VARCHAR(255),
-                    category VARCHAR(255),
-                    photographer_code VARCHAR(10),
-                    FOREIGN KEY(photographer_code) REFERENCES photographers(photographer_code)
-                    )""")
-    connection.execute("""CREATE TABLE new_articles (
-                    a_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    content VARCHAR(255),
-                    keywords VARCHAR(255),
-                    title VARCHAR(255),
-                    category VARCHAR(255),
-                    writer_code VARCHAR(10),
-                    FOREIGN KEY(writer_code) REFERENCES writers(writer_code)
-                    )""")
-    connection.execute("""INSERT INTO new_images (image_path, title, tags, description, category, photographer_code)
-                    SELECT image_path, title, tags, description, category, photographer_code
-                    FROM images
-                    """)
-    connection.execute("""INSERT INTO new_articles (content, keywords, title, category, writer_code)
-                    SELECT content, keywords, title, category, writer_code
-                    FROM articles
-                    """)
-    connection.execute("DROP TABLE images")
-    connection.execute("DROP TABLE articles")
-    connection.execute("ALTER TABLE new_images RENAME TO images")
-    connection.execute("ALTER TABLE new_articles RENAME TO articles")
-
-    # Commit and close the connection
-    connection.commit()
-    connection.close()
-        
+        with db.session.begin():
+            db.session.bulk_insert_mappings(Photographer, photographers_data)
+            db.session.bulk_insert_mappings(Writer, writers_data)
+            db.session.bulk_insert_mappings(Image, images_data)
+            db.session.bulk_insert_mappings(Article, articles_data)
