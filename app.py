1af73c8ec1890a253dcbbcd4f60ad74de366688b
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-06-29 01:53:11 +0330

    Modifying app.py to render the other pages

--- app.py
+++ app.py
@@ -23,15 +23,39 @@ def images():
     return render_template('images.html', images=images, categories=categories, selected_category=selected_category)
 
 
-
-@app.route('/articles')
+@app.route('/articles', methods=['GET', 'POST'])
 def articles():
-    return render_template('articles.html')
+    selected_category = request.form.get('category', 'All')
+    categories = db.session.query(Article.category.distinct()).all()
+    categories = [category[0] for category in categories]  # Extract category names from tuples
+
+    if selected_category == 'All':
+        articles = Article.query.all()
+    else:
+        articles = Article.query.filter_by(category=selected_category).all()
+
+    return render_template('articles.html', articles=articles, categories=categories, selected_category=selected_category)
 
 
-@app.route('/writer-photographer')
-def writer_photographer():
-    return render_template('writer-photographer.html')
+@app.route('/creators')
+def creators():
+    photographers = Photographer.query.all()
+    writers = Writer.query.all()
+    return render_template('creators.html', photographers=photographers, writers=writers)
+
+
+@app.route('/photographer_detail/<code>')
+def photographer_detail(code):
+    photographer = Photographer.query.filter_by(photographer_code=code).first()
+    images = Image.query.filter_by(photographer_code=code).all()
+    return render_template('photographer_detail.html', photographer=photographer, images=images)
+
+
+@app.route('/writer_detail/<code>')
+def writer_detail(code):
+    writer = Writer.query.filter_by(writer_code=code).first()
+    articles = Article.query.filter_by(writer_code=code).all()
+    return render_template('writer_detail.html', writer=writer, articles=articles)
 
 
 if __name__ == "__main__":
